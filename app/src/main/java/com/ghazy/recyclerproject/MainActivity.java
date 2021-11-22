2e98ab50f3b86929b2ddf4f2a721a1101887fd64
GhazyM83 <iamghazy83@gmail.com>
2021-11-22 12:29:47 +0200

    Added recyclerView and its data. Bug in showing all data.

--- app/src/main/java/com/ghazy/recyclerproject/MainActivity.java
+++ app/src/main/java/com/ghazy/recyclerproject/MainActivity.java
@@ -1,16 +1,35 @@
 package com.ghazy.recyclerproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private RecyclerView rvAllSongs;
+    Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // data to populate the RecyclerView with
+        ArrayList<String> songNames = new ArrayList<>();
+        songNames.add("thank u, next");
+        songNames.add("Happier Than Ever");
+        songNames.add("Ana Majnoona");
+        songNames.add("Smack that");
+        songNames.add("Motn Gahar");
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.rvAllSongsMain);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new Adapter(this, songNames);
+        recyclerView.setAdapter(adapter);
     }
 }
